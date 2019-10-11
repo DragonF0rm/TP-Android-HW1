425e01241c14afa88c97d274b4bcde6b78bc3369
Maxim Uymin <m.uymin@corp.mail.ru>
2019-10-12 00:36:05 +0300

    App first version created

--- app/src/androidTest/java/com/example/tp_android_hw1/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/tp_android_hw1/ExampleInstrumentedTest.java
@@ -1,6 +1,7 @@
 package com.example.tp_android_hw1;
 
 import android.content.Context;
+
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
